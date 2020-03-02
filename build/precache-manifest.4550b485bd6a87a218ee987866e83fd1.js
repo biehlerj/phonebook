491beb2014ae7cbc54455a166d2668e1569365f1
Jacob Biehler <biehlerj@gmail.com>
2020-03-02 13:19:35 -0800

    uibuild

---
+++ build/precache-manifest.4550b485bd6a87a218ee987866e83fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd75a7157236a21430b27edef0c4555c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aa95e9a38002590d240",
+    "url": "/static/css/main.c6d5ae2f.chunk.css"
+  },
+  {
+    "revision": "0d4861cc8ca764ed5c63",
+    "url": "/static/js/2.1e3dd35b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1e3dd35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa95e9a38002590d240",
+    "url": "/static/js/main.7ea68299.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
