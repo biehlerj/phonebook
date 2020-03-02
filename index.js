fe710de175bcbd27403e98e14dbb78d121971170
Jacob Biehler <biehlerj@gmail.com>
2020-03-02 11:54:44 -0800

    Adding cors to package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 morgan.token('body', (req, res) => {
-  return JSON.stringify(req.body);
+    return JSON.stringify(req.body);
 });
 
 const app = express();
@@ -14,54 +14,54 @@ app.use(cors());
 app.use(express.static('build'));
 
 let phonebook = [
-  {
-    'name': 'Ada Lovelace',
-    'number': '123456789',
-    'id': 1
-  },
-  {
-    'name': 'Dan Abramov',
-    'number': '12-43-234345',
-    'id': 2
-  },
-  {
-    'name': 'Mary Poppendieck',
-    'number': '39-23-6423122',
-    'id': 3
-  },
-  {
-    'name': 'Jacob Biehler',
-    'number': '8584845307',
-    'id': 6
-  },
-  {
-    'name': 'henlo',
-    'number': '0987654321',
-    'id': 7
-  },
-  {
-    'name': 'hello',
-    'number': '1234567890',
-    'id': 8
-  },
-  {
-    'name': 'wowee',
-    'number': '159753456',
-    'id': 9
-  },
-  {
-    'name': 'ejwpajfio',
-    'number': '1919189',
-    'id': 10
-  }
+    {
+        'name': 'Ada Lovelace',
+        'number': '123456789',
+        'id': 1
+    },
+    {
+        'name': 'Dan Abramov',
+        'number': '12-43-234345',
+        'id': 2
+    },
+    {
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122',
+        'id': 3
+    },
+    {
+        'name': 'Jacob Biehler',
+        'number': '8584845307',
+        'id': 6
+    },
+    {
+        'name': 'henlo',
+        'number': '0987654321',
+        'id': 7
+    },
+    {
+        'name': 'hello',
+        'number': '1234567890',
+        'id': 8
+    },
+    {
+        'name': 'wowee',
+        'number': '159753456',
+        'id': 9
+    },
+    {
+        'name': 'ejwpajfio',
+        'number': '1919189',
+        'id': 10
+    }
 ];
 
 const generateId = (min) => {
-  const max = Math.floor(50000000000000);
-  min = Math.ceil(min);
-  const generatedId = Math.floor(Math.random() * (max - min)) + min;
+    const max = Math.floor(50000000000000);
+    min = Math.ceil(min);
+    const generatedId = Math.floor(Math.random() * (max - min)) + min;
 
-  return generatedId;
+    return generatedId;
 };
 
 app.get('/api', (request, response) => response.send('<h1>Hello World!</h1>'));
@@ -69,64 +69,64 @@ app.get('/api', (request, response) => response.send('<h1>Hello World!</h1>'));
 app.get('/api/persons', (request, response) => response.json(phonebook));
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  let name;
-  let number;
+    const body = request.body;
+    let name;
+    let number;
 
-  if (body.name) {
-    name = body.name;
-  } else {
-    return response.status(400).json({ error: 'Name is missing' });
-  }
+    if (body.name) {
+        name = body.name;
+    } else {
+        return response.status(400).json({error: 'Name is missing'});
+    }
 
-  if (body.number) {
-    number = body.number;
-  } else {
-    return response.status(400).json({ error: 'Number is missing' });
-  }
+    if (body.number) {
+        number = body.number;
+    } else {
+        return response.status(400).json({error: 'Number is missing'});
+    }
 
-  const duplicateName = phonebook.filter(person => person.name === name);
-  if (duplicateName.length > 0) {
-    return response.status(400).json({ error: 'Name must be unique' });
-  }
+    const duplicateName = phonebook.filter(person => person.name === name);
+    if (duplicateName.length > 0) {
+        return response.status(400).json({error: 'Name must be unique'});
+    }
 
-  const person = {
-    name: name,
-    number: number,
-    id: generateId(phonebook.length),
-  };
+    const person = {
+        name: name,
+        number: number,
+        id: generateId(phonebook.length),
+    };
 
-  phonebook = phonebook.concat(person);
+    phonebook = phonebook.concat(person);
 
-  response.json(person);
+    response.json(person);
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = phonebook.find(person => person.id === id);
+    const id = Number(request.params.id);
+    const person = phonebook.find(person => person.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
+    const id = Number(request.params.id);
 
-  phonebook = phonebook.filter(person => person.id !== id);
-  response.status(204).end();
+    phonebook = phonebook.filter(person => person.id !== id);
+    response.status(204).end();
 });
 
 app.get('/info', (request, response) => {
-  const date = new Date();
+    const date = new Date();
 
-  response.send(`Phonebook has info for ${phonebook.length} people\n\n${date}`);
+    response.send(`Phonebook has info for ${phonebook.length} people\n\n${date}`);
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknownEndpoint' });
+    response.status(404).send({error: 'unknownEndpoint'});
 };
 
 app.use(unknownEndpoint);
