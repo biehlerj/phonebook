ec0d1407e4baa2b1535f807737bfa5bd5a76ef71
Jacob Biehler <biehlerj@gmail.com>
2020-03-02 13:07:21 -0800

    uibuild

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ const generateId = (min) => {
     return generatedId;
 };
 
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+
 app.get('/api/persons', (request, response) => response.json(phonebook));
 
 app.post('/api/persons', (request, response) => {
